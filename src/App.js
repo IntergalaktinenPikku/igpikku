b232b63c9dc24987863267ebc1b84c2d4958d05a
Tero Tapio <tero.tapio@helsinki.fi>
2020-04-19 10:45:54 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Super pikku Blaa blaa blaa...
         </a>
       </header>
     </div>
